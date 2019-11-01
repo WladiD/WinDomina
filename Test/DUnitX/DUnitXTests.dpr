a2c631121c0a662bfb0827f98032d089b14bf9c2
WladiD <furevest@gmail.com>
2019-11-01 10:43:09 +0100

    DUnitX hinzugefügt

---
+++ Test/DUnitX/DUnitXTests.dpr
@@ -0,0 +1,97 @@
+program DUnitXTests;
+
+///// UI Selection - Pick only 1! //////////////////////////////
+{$DEFINE UseVCL}
+{DEFINE UseFMX}
+{DEFINE UseWinConsole}
+////////////////////////////////////////////////////////////////
+
+
+{$IFDEF UseWinConsole}
+{$DEFINE UseConsole}
+{$ENDIF}
+
+{$IFDEF UseConsole}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  {$IFDEF UseVCL}
+  VCL.Forms,
+  DUnitX.Loggers.GUI.VCL,
+  {$ENDIF }
+  {$IFDEF UseFMX}
+  FMX.Forms,
+  {$ENDIF }
+  {$IFDEF UseConsole}
+  DUnitX.ConsoleWriter.Base,
+  {$ENDIF }
+  {$IFDEF UseWinConsole}
+  DUnitX.Windows.Console,
+  {$ENDIF }
+  System.SysUtils,
+  DUnitX.Generics,
+  DUnitX.InternalInterfaces,
+  DUnitX.WeakReference,
+  DUnitX.FixtureResult,
+  DUnitX.RunResults,
+  DUnitX.Test,
+  DUnitX.TestFixture,
+  DUnitX.TestFramework,
+  DUnitX.TestResult,
+  DUnitX.TestRunner,
+  DUnitX.Utils,
+  DUnitX.IoC,
+  DUnitX.MemoryLeakMonitor.Default,
+  WindowMatchSnapTest in 'Tests\WindowMatchSnapTest.pas',
+  WinDomina.WindowMatchSnap in '..\..\Source\WinDomina.WindowMatchSnap.pas';
+
+{$R *.res}
+
+/////////////////////////////////////////////////////////////////////////
+{$IFDEF UseVCL}
+begin
+  Application.Initialize;
+  Application.CreateForm(TGUIVCLTestRunner, GUIVCLTestRunner);
+  Application.Run;
+{$ENDIF}
+/////////////////////////////////////////////////////////////////////////
+{$IFDEF UseFMX}
+begin
+  Application.Initialize;
+  Application.CreateForm(TGUIXTestRunner, GUIXTestRunner);
+  Application.Run;
+{$ENDIF}
+/////////////////////////////////////////////////////////////////////////
+{$IFDEF UseConsole}
+var
+  runner : ITestRunner;
+  results : IRunResults;
+  logger : ITestLogger;
+  nunitLogger : ITestLogger;
+
+begin
+   try
+      //Create the runner
+      runner := TDUnitX.CreateRunner;
+      runner.UseRTTI := True;
+      //tell the runner how we will log things
+      logger := TDUnitXConsoleLogger.Create(true);
+      nunitLogger := TDUnitXXMLNUnitFileLogger.Create;
+      runner.AddLogger(logger);
+      runner.AddLogger(nunitLogger);
+
+      //Run tests
+      results := runner.Execute;
+
+      System.Write('Done.. press <Enter> key to quit.');
+      System.Readln;
+
+   except
+      on E: Exception do
+         System.Writeln(E.ClassName, ': ', E.Message);
+   end;
+{$ENDIF}
+/////////////////////////////////////////////////////////////////////////
+
+end.
---
+++ Test/DUnitX/Tests/WindowMatchSnapTest.pas
@@ -0,0 +1,89 @@
+﻿unit WindowMatchSnapTest;
+
+interface
+
+uses
+  DUnitX.TestFramework,
+  System.Types,
+  System.SysUtils,
+
+  WindowEnumerator,
+  WinDomina.WindowMatchSnap;
+
+type
+  [TestFixture]
+  TWindowMatchSnapTest = class
+  private
+    FWindowList: TWindowList;
+
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+
+    [Test]
+    procedure CreateAndDestroy;
+    [Test]
+    [TestCase('A', '')]
+    procedure MatchLeftSimple(RefLeft, RefTop, RefRight, RefBottom: Integer);
+
+
+    // Test with TestCase Attribute to supply parameters.
+//    [Test]
+//    [TestCase('TestA','1,2')]
+//    [TestCase('TestB','3,4')]
+//    procedure Test2(const AValue1 : Integer;const AValue2 : Integer);
+  end;
+
+implementation
+
+procedure TWindowMatchSnapTest.Setup;
+begin
+  FWindowList := TWindowList.Create;
+end;
+
+procedure TWindowMatchSnapTest.TearDown;
+begin
+  FWindowList.Free;
+end;
+
+procedure TWindowMatchSnapTest.CreateAndDestroy;
+var
+  WMS: TWindowMatchSnap;
+  List: TWindowList;
+begin
+  WMS := nil;
+  List := TWindowList.Create;
+  try
+    List.Add(TWindow.Create);
+    // Hier will ich sicherstellen, dass die an TWindowMatchSnap übergebene TWindowList
+    // dort nicht freigegeben wird.
+    WMS := TWindowMatchSnap.Create(Rect(10, 10, 20, 20), Rect(0, 0, 600, 600), List);
+    FreeAndNil(WMS);
+    Assert.IsNotNull(List[0]);
+
+    List.Add(TWindow.Create);
+    Assert.AreEqual(2, List.Count);
+  finally
+    List.Free;
+    WMS.Free;
+  end;
+end;
+
+procedure TWindowMatchSnapTest.MatchLeftSimple(RefLeft, RefTop, RefRight, RefBottom: Integer);
+var
+  List: TWindowList;
+begin
+
+end;
+
+//procedure TWindowMatchSnapTest.Test2(const AValue1 : Integer;const AValue2 : Integer);
+//begin
+//
+//end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TWindowMatchSnapTest);
+
+end.
