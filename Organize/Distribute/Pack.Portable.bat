9df0678141c84143773f3f005fed15f31648ca1f
WladiD <furevest@gmail.com>
2020-05-02 08:53:25 +0200

    Base for automatic distributions

---
+++ Organize/Distribute/Pack.Portable.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set self_path=%~dp0
+set tools_path=%self_path%Tools\
+
+echo %self_path%
---
+++ Organize/Distribute/Tools/GetVersion.cmd
@@ -0,0 +1,17 @@
+@echo off
+setlocal enableextensions
+
+set "file=%~1"
+if not defined file goto :eof
+if not exist "%file%" goto :eof
+
+set "vers="
+FOR /F "tokens=2 delims==" %%a in ('
+	wmic datafile where name^="%file:\=\\%" get Version /value 
+') do set "vers=%%a"
+
+:echo(%file% = %vers% 
+
+endlocal & set "vers=%vers%"
+
+echo %vers:~0,-2%
